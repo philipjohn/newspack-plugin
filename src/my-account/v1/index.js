90990aaeedf72b2dd55e7711ae47ea1da221ce3d
Derrick Koo <dkoo@users.noreply.github.com>
2025-05-27 15:06:34 -0600

    feat(my-account): custom confirmation modal flows (#3985)
    * feat(my-account): confirmation modal for delete account/cancel subscription
    
    * chore: remove redundant comment
    
    * feat(my-account): new template for Account Settings
    
    * fix: only generate password key after form submission
    
    * feat: new delete account button
    
    * feat(my-account): delete account modal-based flow
    
    * fix: style dependency
    
    * refactor: move modals.js to Newspack UI instead of My Account assets
    
    * chore: order of use statements
    
    * fix: padding styles for My Account pages without nav sidebar
    
    * fix: avoid double padding on content area
    
    * fix: re-add renamed templates
    
    * fix: filename for verify.php template part
    
    * fix: force email-based password reset if without password
    
    * fix: custom password reset modal when logged in
    
    * fix: check password reset key before showing reset modal
    
    * fix: avoid double error message if invalid reset key
    
    * feat: update password strength
    
    * feat: update notice
    
    * feat: update password display icons
    
    * fix: strip tags from admin notices
    
    * style: remove unnecessary class names in modal form
    
    * style: standardize password strength error styles
    
    * refactor: move password functionality to separate class
    
    * fix: verify password reset nonce
    
    * refactor: use guard clauses to check conditions for all functions
    
    * fix: filter show-reset-form param with expected boolean type
    
    * fix: update messaging when resetting existing password
    
    * fix: show My Account custom page template for all users, not just readers
    
    * fix: only show password UI for reader users
    
    * fix: only show Manage Subscriptions/Newsletters if user has any
    
    * refactor: use Reader_Data to get subscription/donation/newsletter info
    
    ---------
    
    Co-authored-by: Thomas Guillot <thomas@automattic.com>

--- src/my-account/v1/index.js
+++ src/my-account/v1/index.js
@@ -2,5 +2,10 @@
  * Customizations for Newspack My Account pages: v1.x.x.
  */
 
+/**
+ * Internal dependencies.
+ */
 import './navigation';
+import './account-settings';
+import './subscriptions';
 import './style.scss';
