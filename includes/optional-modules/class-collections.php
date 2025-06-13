48466183294a87689730497b805fa329deb232f7
Ramon Corrales <rbcorrales@users.noreply.github.com>
2025-06-13 14:55:27 -0500

    feat(collections): register collection section taxonomy (#4003)
    Includes changes from #4017 and #4018.

--- includes/optional-modules/class-collections.php
+++ includes/optional-modules/class-collections.php
@@ -9,15 +9,12 @@ namespace Newspack;
 
 defined( 'ABSPATH' ) || exit;
 
-require_once __DIR__ . '/../collections/traits/trait-hook-management.php';
-require_once __DIR__ . '/../collections/class-post-type.php';
-require_once __DIR__ . '/../collections/class-collection-taxonomy.php';
-require_once __DIR__ . '/../collections/class-collection-category-taxonomy.php';
-require_once __DIR__ . '/../collections/class-sync.php';
-
+use Newspack\Collections\Enqueuer;
 use Newspack\Collections\Post_Type;
 use Newspack\Collections\Collection_Taxonomy;
 use Newspack\Collections\Collection_Category_Taxonomy;
+use Newspack\Collections\Collection_Section_Taxonomy;
+use Newspack\Collections\Post_Meta;
 
 /**
  * Collections module for managing print editions and other collections.
@@ -39,9 +36,13 @@ class Collections {
 			return;
 		}
 
+		// Initialize classes.
+		Enqueuer::init();
 		Post_Type::init();
 		Collection_Taxonomy::init();
 		Collection_Category_Taxonomy::init();
+		Collection_Section_Taxonomy::init();
+		Post_Meta::init();
 	}
 
 	/**
